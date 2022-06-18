89b20affce394d69023d5485911f8132a3053b8b
alina-platonova <aliina.platonova@gmail.com>
2022-06-18 22:11:18 +0300

    Finished HomeWorkTwo.java

--- src/main/java/homeworkTwo/HomeWorkTwo.java
+++ src/main/java/homeworkTwo/HomeWorkTwo.java
@@ -113,16 +113,53 @@ public class HomeWorkTwo {
 
         // 5. uzdevums
         System.out.println("\n5. uzdevums");
+        printBusinessCard();
+        printBusinessCard();
+        printBusinessCard();
 
         // 6. uzdevums
         System.out.println("\n6. uzdevums");
+        printBusinessCardTwo("Līga", "Kalniņa", "+371 12312313", 1965);
+        printBusinessCardTwo("Juris", "Vītols", "+371 5142431", 1930);
 
         // 7. uzdevums
         System.out.println("\n7. uzdevums");
+        int summa = sum(7, 4);
+        System.out.println(summa);
 
         // 8. uzdevums
         System.out.println("\n8. uzdevums");
-
-
+        System.out.println(average(10,2,4));
     }
+
+
+    // Metodes
+    static void printBusinessCard() {
+        System.out.println("Vizītkarte");
+        System.out.println("############");
+        System.out.println("Vārds: Jānis");
+        System.out.println("Uzvārds: Bērziņš");
+        System.out.println("Telefona numurs: +371 12345678");
+        System.out.println("Dzimšanas gads: 1990");
+        System.out.println("############");
+    }
+
+    static void printBusinessCardTwo(String name, String surname, String telephone, int dateOfBirth) {
+        System.out.println("Vizītkarte");
+        System.out.println("############");
+        System.out.println("Vārds: " + name);
+        System.out.println("Uzvārds: " + surname);
+        System.out.println("Telefona numurs: " + telephone);
+        System.out.println("Dzimšanas gads: " + dateOfBirth);
+        System.out.println("############");
+    }
+
+    static int sum(int a, int b) {
+        return a+b;
+    }
+
+    static double average(double c, double d, double e) {
+        return (c+d+e)/3;
+    }
+
 }
